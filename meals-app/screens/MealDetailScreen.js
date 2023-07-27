5225dcad86927e1c13156437de0f9074425bfba4
Joseph <anselljoseph19@gmail.com>
2023-07-27 08:13:01 +0100

    saving progress with meals app

--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,16 +1,57 @@
-import { FlatList, View, Text } from "react-native";
+import { Image, View, Text, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
-import CategoryGridTile from "../components/CategoryGridTitle";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 function MealDetailScreen({ route }) {
-  const { title, mealId } = route.params;
+  const { mealId } = route.params;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View>
-      <Text>
-        {title}, {mealId}
-      </Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+  },
+  detailText: {
+    color: "black",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
