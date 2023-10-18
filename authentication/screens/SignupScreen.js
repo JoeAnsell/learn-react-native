07c34668124ab8f4de5cc587cce9af949b1ae7cd
Joseph <anselljoseph19@gmail.com>
2023-10-18 20:32:06 +0100

    fixed auth bug with authContext

--- authentication/screens/SignupScreen.js
+++ authentication/screens/SignupScreen.js
@@ -3,13 +3,24 @@ import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { useState } from "react";
 
 import { createUser } from "../util/auth";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function signUpHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your credientials or try aian later!"
+      );
+    }
     setIsAuthenticating(false);
   }
 
--- authentication/util/auth.js
+++ authentication/util/auth.js
@@ -2,13 +2,25 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyDoWk9JutReVU0mBWdsF - IBIXSfQaGYYvQ";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+
+  console.log(response.data);
+  return token;
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  return authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
