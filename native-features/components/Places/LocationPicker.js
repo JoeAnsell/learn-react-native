ce7a6e92b1b46af4c474aaaa379bd7b57bafca22
Joseph <anselljoseph19@gmail.com>
2023-11-20 20:20:09 +0000

    saving progress

--- native-features/components/Places/LocationPicker.js
+++ native-features/components/Places/LocationPicker.js
@@ -1,4 +1,4 @@
-import { Alert, View, StyleSheet } from "react-native";
+import { Image, Alert, View, StyleSheet, Text } from "react-native";
 import {
   getCurrentPositionAsync,
   useForegroundPermissions,
@@ -6,8 +6,12 @@ import {
 } from "expo-location";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
+import { useState } from "react";
+import { getMapPreview } from "../../util/location";
 
 function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+
   const [locationPermissionsInformation, requestPermission] =
     useForegroundPermissions();
 
@@ -39,13 +43,31 @@ function LocationPicker() {
     }
 
     const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
     console.log(location);
   }
 
   function pickOnMapHandler() {}
+
+  let locationPreview = <Text>No location pciked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton icon="location" onPress={getLocationHandler}>
           Locate User
@@ -69,10 +91,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden",
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 4,
   },
 });
