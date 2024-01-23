540524f4683be7330be6b919cc3c56bf7455ecf4
Joseph <anselljoseph19@gmail.com>
2024-01-23 12:01:24 +0000

    update env local files

--- native-features/util/location.js
+++ native-features/util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBX9DbWuY8vrx6yb7J7cJyGmdR88yWfuMA";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
